9143f148e6fed582b5b2ad9fa7f6c48e244caeb3
Leonardo M <leomagari_os@hotmail.com>
2018-06-06 15:27:54 -0300

    tpe terminado

--- segundaEntrega/Test.java
+++ segundaEntrega/Test.java
@@ -6,19 +6,20 @@ import java.util.ArrayList;
 
 public class Test {
 	public static void main(String[] args){
-		
-		String archivoEntrada="D:\\prog3\\dataset-2da-parte\\dataset1.csv";
+		//configurar primero
+		String archivoEntrada="D:\\prog3\\dataset-2da-parte\\dataset4.csv";
 		String separador=",";
 		String generoABuscar="viajes";
-
+		Timer t=new Timer();
 		LectorCsv lector=new LectorCsv(archivoEntrada,separador);
 		Grafo g=new Grafo();
 		lector.getBusquedas(g);
 
-//		System.out.println(g.getCantVertices());//para controlar cantidad total vs cantidad del testeo actual
-		
+		System.out.println(g.getCantVertices());//para controlar cantidad total vs cantidad del testeo actual
+		//para testear cada servicio descomentar 
 //		System.out.println("Servicio 1");
 //		int n=11;
+//		t.start();
 //		ArrayList<Arista> res=g.getGenerosMasBuscados(new Vertice(generoABuscar),n);//servicio 1
 //		if(res.size()==0){
 //			System.out.println("Se ingreso un genero erroneo o el genero no tiene busquedas siguientes asociadas.");
@@ -27,8 +28,13 @@ public class Test {
 //				System.out.println(res.get(i).getOrigen().getEtiqueta()+"->"+res.get(i).getDestino().getEtiqueta()+" Peso: "+res.get(i).getPeso());
 //			}
 //		}
+//		System.out.println("la ejecucion del servicio 1 tardo"+t.stop());
+//		System.out.println("Cantidad de Aristas"+g.getCantidadAristas());
+//		System.out.println("Cantidad de Busquedas"+lector.getCantBusquedas());
 //		
+//		////////////////////////
 //		System.out.println("Servicio 2");
+//		t.start();
 //		ArrayList<Vertice> res=g.generosBuscadosAfter(new Vertice(generoABuscar));//servicio2
 //		
 //		if(res!=null){
@@ -39,17 +45,21 @@ public class Test {
 //		}else{
 //			System.out.println("El genero por el que busca no existe o no tiene vinculada mas busquedas.");
 //		}
-//		
-//		System.out.println("Servicio 3");
-//		Grafo res=g.getGrafoGenerosAfines();
-//		System.out.println("Cantidad de vertices original: "+g.getCantVertices());
-//		System.out.println("Cantidad de vertices Grafo afin: "+res.getCantVertices());
-//		System.out.println(res);
-
-		
-//otros tests		
-//		System.out.println(g);
+//		System.out.println("la ejecucion del servicio 2 tardo"+t.stop());
 //		System.out.println("Cantidad de Aristas"+g.getCantidadAristas());
 //		System.out.println("Cantidad de Busquedas"+lector.getCantBusquedas());
+//		
+//		////////////////////////
+		System.out.println("Servicio 3");
+		t.start();
+		Grafo res=g.getGrafoGenerosAfines();
+		System.out.println("Cantidad de vertices original: "+g.getCantVertices());
+		System.out.println("Cantidad de vertices Grafo afin: "+res.getCantVertices());
+//		System.out.println(res);
+		System.out.println("la ejecucion del servicio 2 tardo"+t.stop());
+		System.out.println("Cantidad de Aristas"+g.getCantidadAristas());
+		System.out.println("Cantidad de Busquedas"+lector.getCantBusquedas());
+
+
 	}
 }
